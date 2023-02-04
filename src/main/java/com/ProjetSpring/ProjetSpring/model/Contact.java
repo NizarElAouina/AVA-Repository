11d44caf2c473296a463824eaf4bdc502c9888a2
NizarElAouina <93288298+NizarElAouina@users.noreply.github.com>
2023-02-04 21:45:24 +0100

    Add files via upload

---
+++ src/main/java/com/ProjetSpring/ProjetSpring/model/Contact.java
@@ -0,0 +1,58 @@
+package com.ProjetSpring.ProjetSpring.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name =  "Contact", uniqueConstraints = @UniqueConstraint(columnNames = "idMessage"))
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private int idMessage;
+    private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "idUser", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    public int getIdMessage() {
+        return idMessage;
+    }
+
+    public void setIdMessage(int idMessage) {
+        this.idMessage = idMessage;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Contact() {
+
+    }
+
+    public Contact(String message) {
+        super();
+        this.message=message;
+    }
+
+
+}

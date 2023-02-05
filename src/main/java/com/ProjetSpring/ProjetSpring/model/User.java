b2e072c70af52eb9ecfac59cc142bd63238596a2
NizarElAouina <93288298+NizarElAouina@users.noreply.github.com>
2023-02-05 14:18:14 +0100

    Add files via upload

--- src/main/java/com/ProjetSpring/ProjetSpring/model/User.java
+++ src/main/java/com/ProjetSpring/ProjetSpring/model/User.java
@@ -1,21 +1,13 @@
 package com.ProjetSpring.ProjetSpring.model;
-
 import jakarta.persistence.Entity;
-//import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//import jakarta.persistence.JoinColumn;
-//import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-
-
-
 @Entity
 @Table(name =  "User", uniqueConstraints = @UniqueConstraint(columnNames = "idUser"))
 public class User{
-
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private int idUser;
